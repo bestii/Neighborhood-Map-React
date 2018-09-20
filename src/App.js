6dfaa70ecea62ab041ba1ee3a13ff687d36ee63d
Bestin John <bjohn@mcfadyen.com>
2018-09-21 00:12:39 +0530

    Setup the Header

--- src/App.js
+++ src/App.js
@@ -4,9 +4,27 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+    <header>
+        <div className="sticky-header box-shadow">
+            <div className="header-menu text-center d-inline-block h-100 w-10">
+                <i className="fa fa-bars fs-24 cursor-pointer hamburger" id="hamburger-ico"></i>
+            </div>
+            <div className="header-content text-center d-inline-block h-100 w-80">
+                <h1 className="fs-20">Neigborhood Map</h1>
+            </div>
+        </div>
+        <nav className="menu menu-vertical menu-left box-shadow h-100" id="slider-menu">
+            <i id="close-ico" className="fa fa-close fs-24 cursor-pointer close"></i>
+            <ul className="location-list">
+                <li>Celery seakale</li>
+                <li>Dulse daikon</li>
+                <li>Zucchini garlic</li>
+                <li>Catsear azuki bean</li>
+                <li>Dandelion bunya</li>
+                <li>Rutabaga</li>
+            </ul>
+        </nav>
+    </header>
     );
   }
 }
