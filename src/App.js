43cdecd2282fa6d6c8fd6887b7820571c122c0c2
Bestin John <bestinjohn1993@gmail.com>
2018-10-01 17:11:17 +0530

    Added InfoWindow

--- src/App.js
+++ src/App.js
@@ -7,45 +7,6 @@ import Footer from './components/Footer';
 
 
 class App extends Component {
-  /*   state = {
-      locations: [
-        {
-          lat: 9.96691498300256,
-          lng: 76.3181688690639,
-          name: 'Priceless Factory Outlet'
-        },
-        {
-          lat: 9.967080995007404,
-          lng: 76.31812928698632,
-          name: 'K R Bakes',
-        },
-        {
-          lat: 9.965904,
-          lng: 76.317918,
-          name: 'Broad Bean Hotel'
-        },
-        {
-          lat: 9.96387638705125,
-          lng: 76.31343589989426,
-          name: 'Welcare Hospital'
-        },
-        {
-          lat: 9.96387638705125,
-          lng: 76.31343589989426,
-          name: 'Golden dragon'
-        },
-        {
-          lat: 9.965679795023949,
-          lng: 76.31514928813195,
-          name: 'Hotel Emarald'
-        },
-        {
-          lat: 9.962810458403629,
-          lng: 76.31305656345499,
-          name: 'Toch Public School'
-        }
-      ]
-    } */
 
   state = {
     locations: []
@@ -65,17 +26,34 @@ class App extends Component {
 
   // Google Maps callback function
   initMap = () => {
+
+    // Map object
     let map = new window.google.maps.Map(document.getElementById('map'), {
       zoom: 16,
       center: { lat: 9.965459, lng: 76.313982 }
     });
+    
+    // Infowindow object
+    let infowindow = new window.google.maps.InfoWindow();
 
+    // Add marker for each location
     this.state.locations.map(location => {
+
+      //Create Marker object
       let marker = new window.google.maps.Marker({
         map: map,
         draggable: true,
         animation: window.google.maps.Animation.DROP,
         position: { lat: location.venue.location.lat, lng: location.venue.location.lng }
+      });
+
+      // setup the content string for each location
+      let contentString = `${location.venue.name}`;
+
+      // Add event listener to each marker to open infowindow
+      marker.addListener('click', function() {
+        infowindow.setContent(contentString);
+        infowindow.open(map, marker);
       });
     });
 
@@ -95,12 +73,13 @@ class App extends Component {
     axios.get(locationEndPoint, {
       params: parameters
     }).then(response => {
-      console.log(response.data.response.groups[0].items);
+
       this.setState(
         { locations: response.data.response.groups[0].items },
         // Callback function
         this.setupGoogleMaps()
       );
+
     }).catch(error => {
       console.error("Get request failed", error);
     })
